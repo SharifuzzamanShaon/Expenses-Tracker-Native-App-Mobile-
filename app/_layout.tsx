00352f39cf021755ac4b3abaf2057e55f80157e9
sharifuzzamanshaon <shasan2200@gmail.com>
2025-07-02 19:31:02 +0600

    fix: clean up imports and ensure Toast component is rendered in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-import { AuthProvider } from '@/context/authContext'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { AuthProvider } from "./../context/authContext";
+import Toast from "react-native-toast-message";
 
 const StackLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>
-}
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+};
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <StackLayout />
+      <Toast position="top" bottomOffset={20} />
     </AuthProvider>
-  )
+  );
 }
 
-
-
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
