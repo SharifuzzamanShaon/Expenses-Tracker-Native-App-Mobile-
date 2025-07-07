82ebfa1b323bdf414d03c50116bd48657f930fb0
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-07 20:40:27 +0600

    refactor: reorganize imports and update layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import { Stack } from "expo-router";
-import { AuthProvider } from "./../context/authContext";
+import React from "react";
+import { StyleSheet } from "react-native";
 import Toast from "react-native-toast-message";
+import { AuthProvider } from "./../context/authContext";
 
 const StackLayout = () => {
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
