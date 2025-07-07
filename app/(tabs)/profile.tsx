1cf821d21cda9cc3726fb934376194e5dd273b5a
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-07 20:40:53 +0600

    feat: implement logout functionality in profile component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import Button from '@/components/Button'
+import Typo from '@/components/Typo'
+import { myAuth } from '@/config/firebase'
+import { colors } from '@/constants/theme'
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 // import { useGlobalContext } from '@/context/authContext';
 
 const Profile = () => {
     // const {user} = useGlobalContext();
+  const handleLogout=async()=>{
+   await signOut(myAuth)
+  }
   return (
     <View>
-      <Text>profile</Text>
-      
+      <Button onPress={handleLogout}>
+        <Typo color={colors.black}>
+          Logout
+        </Typo>
+      </Button>
     </View>
   )
 }
