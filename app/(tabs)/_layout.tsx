fc044f42551245ee0daecbfc24d2a5b86c90a643
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-08 18:42:32 +0600

    feat: implement custom tab layout with screen options

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,19 @@
-import { Tabs } from 'expo-router'
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import CustomTabs from "@/components/CustomTabs";
+import { Tabs } from "expo-router";
+import React from "react";
+import { StyleSheet } from "react-native";
 
 const _layout = () => {
   return (
-    <Tabs></Tabs>
-  )
-}
+    <Tabs tabBar={props => <CustomTabs {...props}  />}  screenOptions={{headerShown: false}}>
+      <Tabs.Screen name="index" options={{title:"Home"}}/>
+      <Tabs.Screen name="wallet" options={{title:"Wallet"}} />
+      <Tabs.Screen name="statistics" options={{title:"Statistics"}} />
+      <Tabs.Screen name="profile" options={{title:"Profile"}} />
+    </Tabs>
+  );
+};
 
-export default _layout
+export default _layout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
