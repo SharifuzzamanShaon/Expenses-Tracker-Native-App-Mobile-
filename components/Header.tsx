503aae590d6b7bf444937a2327ea67f1f767d7d6
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-11 10:11:59 +0600

    feat: enhance Header component to support leftIcon prop and improve layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import Typo from './Typo'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Typo from "./Typo";
 
-const Header = ({title}: {title: string}) => {
+const Header = ({ title, leftIcon }: { title: string; leftIcon: any |"" }) => {
   return (
     <View style={styles.container}>
-      <Typo size={20} fontWeight={400} style={styles.title}>{title}</Typo>
+      {leftIcon && <View style={styles.leftIcon}>{leftIcon}</View>}
+      {title && (
+        <Typo
+          size={20}
+          fontWeight={400}
+          style={{ textAlign: "center", width: leftIcon ? "90%" : "100%" }}
+        >
+          {title}
+        </Typo>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        padding: 10,
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-})
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    paddingTop:10
+  },
+
+  leftIcon: {
+    alignSelf: "flex-start",
+  },
+});
