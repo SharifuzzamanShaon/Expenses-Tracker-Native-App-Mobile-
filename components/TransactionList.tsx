99b014e49f40f0a82cd419d192239a461942f2e7
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-16 18:34:50 +0600

    component added

---
+++ components/TransactionList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Typo from './Typo'
+
+const TransactionList = () => {
+  return (
+    <View>
+      <Typo>TransactionList</Typo>
+    </View>
+  )
+}
+
+export default TransactionList
+
+const styles = StyleSheet.create({})
