36a95be1c9e6f73ccd9421ae25b15fc2da3c09de
sharifuzzamanShaon <shasan2200@gmail.com>
2025-07-17 19:34:57 +0600

    transactionlist comp.

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,15 +1,129 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import Typo from './Typo'
+import { expenseCategories } from "@/constants/data";
+import { colors, radius, spacingY } from "@/constants/theme";
+import { TransactionItemProps, TransactionListType } from "@/types";
+import { verticalScale } from "@/utils/styling";
+import { FlashList } from "@shopify/flash-list";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Loading from "./Loading";
+import Typo from "./Typo";
 
-const TransactionList = () => {
+const TransactionList = ({
+  data,
+  title,
+  loading,
+  emptyListMessage,
+}: TransactionListType) => {
+  const handleClick = () => {};
+  return (
+    <View style={styles.container}>
+      {title && <Typo>TransactionList</Typo>}
+      <View style={styles.list}>
+        <FlashList
+          data={data}
+          renderItem={({ item, index }) => (
+            <TransactionItem
+              item={item}
+              index={index}
+              handleClick={handleClick}
+            />
+          )}
+        />
+      </View>
+      {!loading && data.length === 0 && (
+        <Typo
+          size={15}
+          style={{ textAlign: "center", marginTop: spacingY._17 }}
+        >
+          {emptyListMessage}
+        </Typo>
+      )}
+      {loading && (
+        <View style={{ alignItems: "center" }}>
+          <Loading />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default TransactionList;
+const TransactionItem = ({
+  item,
+  index,
+  handleClick,
+}: TransactionItemProps) => {
+  let category = expenseCategories["utilities"];
+  let IconComponent = category.icon;
   return (
     <View>
-      <Typo>TransactionList</Typo>
+      <TouchableOpacity style={styles.row}>
+        <View style={[styles.icon, { backgroundColor: category.bgColor }]}>
+          {IconComponent && (
+            <IconComponent
+              size={verticalScale(25)}
+              weight="fill"
+              color={colors.white}
+            />
+          )}
+        </View>
+        <View style={styles.categoryDes}>
+          <Typo size={17}>{category.label}</Typo>
+          <Typo
+            size={12}
+            color={colors.neutral400}
+            textProps={{ numberOfLines: 1 }}
+          >
+            {item?.description}
+            Paid bill
+          </Typo>
+        </View>
+        <View style={styles.amountDate}>
+          <Typo fontWeight={"500"} color={colors.rose}>
+            - $23
+          </Typo>
+          <Typo size={13} color={colors.neutral400}>
+            12 jan
+          </Typo>
+        </View>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default TransactionList
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    gap: spacingY._17,
+  },
+  list: {
+    minHeight: 3,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: spacingY._12,
+    marginBottom: spacingY._12,
+    // list with background
+    backgroundColor: colors.neutral800,
+    padding: spacingY._10,
+    paddingHorizontal: spacingY._10,
+    borderRadius: radius._17,
+  },
+  icon: {
+    height: verticalScale(44),
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: radius._12,
+    borderCurve: "continuous",
+  },
+  categoryDes: {
+    flex: 1,
+    gap: 2.5,
+  },
+  amountDate: {
+    alignItems: "flex-end",
+    gap: 3,
+  },
+});
